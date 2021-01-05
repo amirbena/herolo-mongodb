82df514805c122599648f1be4fb72fa118fdc50c
amirbena <amir12061968@gmail.com>
2021-01-05 21:17:45 +0200

    Final Version Heroku

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -2,14 +2,14 @@ const { StatusCodes } = require('http-status-codes')
 const jwt = require('jsonwebtoken');
 
 
-const sendBadRequestStatus= res=>{
-  return res.status(StatusCodes.BAD_REQUEST).send("No authenticated user, please log in");
+const sendBadRequestStatus = res => {
+    return res.status(StatusCodes.BAD_REQUEST).send("No authenticated user, please log in");
 }
 const authentication = (req, res, next) => {
-    
+
     const authorization = req.headers["authorization"];
-    if(authorization) return sendBadRequestStatus(res);
-    const token = req.headers["authorization"].split(" ")[1];
+    if (!authorization) return sendBadRequestStatus(res);
+    const token = authorization.split(" ")[1];
     if (!token) return sendBadRequestStatus(res);
     const verifiedToken = jwt.decode(token);
     req.user = verifiedToken;
